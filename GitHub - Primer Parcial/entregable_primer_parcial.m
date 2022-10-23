c7836c77e5dd2995d3d6fcc92ff5512e434bc39d
Miguel Flores <63883454+stbn17@users.noreply.github.com>
2022-10-22 22:39:22 -0500

    Create entregable_primer_parcial.m

---
+++ GitHub - Primer Parcial/entregable_primer_parcial.m
@@ -0,0 +1,21 @@
+% ROBÓTICA - PRIMER PARCIAL
+% Nombre: Miguel Esteban Flores Sierra
+% Código: 2310949
+% 1. Con sus palabras: 
+%     a. ¿Qué es un robot? 
+%         Es una máquina que puede realizar debidas tareas pre-programadas con muy poca o casi nada de interacción humana, entre ellos se encuentras las máquinas de precisión como los industriales en plantas de ensamblaje, o acabados. , Se espera que en unos años con el avance de la IA y ML, se puedan producir robots semi-conscientes los cuales puedan empezar a tomar decisiones lógicas basadas en su entrenamiento.
+%     b. De ejemplos de actuadores, sensores y elementos terminales en robots
+%         Actuadores:
+%             Actuadores neumáticos como los cilindros de doble efecto.
+%             Motores DC y AC.
+%         Sensores:
+%             Sensores de fin de carrera.
+%             Giroscópio.
+%             Encoders.
+%             Efecto Hall.
+%             Ultrasónico.
+%             Elementos terminales:
+%             Pinzas hidráulicas.
+%             Electroimanes.
+%             Pinza de soldadura de arco.
+%
